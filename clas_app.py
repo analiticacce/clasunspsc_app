0b0736d77c1da4e24e6a75e8f6cacf706793be12
analiticacce <96195617+analiticacce@users.noreply.github.com>
2022-07-05 16:42:49 -0500

    Update clas_app.py

--- clas_app.py
+++ clas_app.py
@@ -3,7 +3,7 @@ from clasunspsc.clasunspsc import recomendador_api
 import pandas as pd
 st.title("Clasificador UNSPSC")
 st.header("GAEC - Colombia Compra Eficiente")
-name = st.text_input("Ingrese productos", "Type Here ...")
+name = st.text_input("Ingrese productos", "")
 if(st.button('Submit')):
     result = recomendador_api(name,4)
     result=pd.DataFrame(result)
