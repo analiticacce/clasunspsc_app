ea6ca91997c2ba284805436830c35501015e2390
analiticacce <96195617+analiticacce@users.noreply.github.com>
2022-07-05 16:16:41 -0500

    Add files via upload

--- clas_app.py
+++ clas_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from clasunspsc.clasunspsc import recomendador_api
 import pandas as pd
 st.title("Clasificador UNSPSC")
-st.header("This is a header")
+st.header("GAEC - Colombia Compra Eficiente")
 name = st.text_input("Ingrese productos", "Type Here ...")
 if(st.button('Submit')):
     result = recomendador_api(name,4)
