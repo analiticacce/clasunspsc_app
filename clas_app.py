319d4b36fd7b96c0abe1f46d5f65eda6dd959d27
analiticacce <96195617+analiticacce@users.noreply.github.com>
2022-07-05 16:44:39 -0500

    Update clas_app.py

--- clas_app.py
+++ clas_app.py
@@ -5,6 +5,6 @@ st.title("Clasificador UNSPSC")
 st.header("GAEC - Colombia Compra Eficiente")
 name = st.text_input("Ingrese productos", "")
 if(st.button('Submit')):
-    result = recomendador_api(name,4)
+    result = recomendador_api(name,10)
     result=pd.DataFrame(result)
     st.table(result)
