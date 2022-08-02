6d64b21f2a2102c181acd59e782a30a650b22cf4
analiticacce <96195617+analiticacce@users.noreply.github.com>
2022-08-02 15:26:28 -0500

    Update clas_app.py

--- clas_app.py
+++ clas_app.py
@@ -9,3 +9,16 @@ if(st.button('Submit')):
     dicc={'Código Producto':dicc['Código Producto'],'Nombre Producto':dicc['Nombre Producto'],'Nombre Clase':dicc['Nombre Clase']}
     result=pd.DataFrame(dicc)
     st.table(result)
+ @st.cache
+ def convert_df(df):
+     # IMPORTANT: Cache the conversion to prevent computation on every rerun
+     return df.to_csv().encode('utf-8')
+
+csv = convert_df(my_large_df)
+
+st.download_button(
+     label="Descargue CSV",
+     data=csv,
+     file_name='large_df.csv',
+     mime='text/csv',
+ )
