8ef0a1324532969ccf106907aec1b01cc277c620
analiticacce <96195617+analiticacce@users.noreply.github.com>
2022-08-02 15:28:42 -0500

    Update clas_app.py

--- clas_app.py
+++ clas_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from clasunspsc.clasunspsc import recomendador_api
 import pandas as pd
+
+@st.cache
+def convert_df(df):
+     # IMPORTANT: Cache the conversion to prevent computation on every rerun
+     return df.to_csv().encode('utf-8')
 st.title("Clasificador UNSPSC")
 st.header("GAEC - Colombia Compra Eficiente")
 name = st.text_input("Ingrese productos", "")
@@ -9,15 +14,8 @@ if(st.button('Submit')):
     dicc={'Código Producto':dicc['Código Producto'],'Nombre Producto':dicc['Nombre Producto'],'Nombre Clase':dicc['Nombre Clase']}
     result=pd.DataFrame(dicc)
     st.table(result)
-    
-@st.cache
-def convert_df(df):
-     # IMPORTANT: Cache the conversion to prevent computation on every rerun
-     return df.to_csv().encode('utf-8')
-
-csv = convert_df(result)
-
-st.download_button(
+    csv = convert_df(result)
+    st.download_button(
      label="Descargue CSV",
      data=csv,
      file_name='large_df.csv',
