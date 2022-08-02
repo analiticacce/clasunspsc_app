77515e8e5d67831a45f92f64b5c97d84a42f9582
analiticacce <96195617+analiticacce@users.noreply.github.com>
2022-08-02 15:27:19 -0500

    Update clas_app.py

--- clas_app.py
+++ clas_app.py
@@ -15,7 +15,7 @@ def convert_df(df):
      # IMPORTANT: Cache the conversion to prevent computation on every rerun
      return df.to_csv().encode('utf-8')
 
-csv = convert_df(my_large_df)
+csv = convert_df(result)
 
 st.download_button(
      label="Descargue CSV",
