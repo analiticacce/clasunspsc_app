b305c92345a5a5251f8e4b8231cc54a3319094b6
analiticacce <96195617+analiticacce@users.noreply.github.com>
2022-08-02 15:18:47 -0500

    Update clas_app.py

--- clas_app.py
+++ clas_app.py
@@ -6,5 +6,4 @@ st.header("GAEC - Colombia Compra Eficiente")
 name = st.text_input("Ingrese productos", "")
 if(st.button('Submit')):
     result = recomendador_api(name,10)
-    result=pd.DataFrame(result)
     st.table(result)
